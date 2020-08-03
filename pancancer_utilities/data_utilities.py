5c4ab9634aaba1d44fac4ddc68613aab7aea67dc
Jake Crawford <jjc2718@gmail.com>
2020-08-03 12:52:52 -0400

    add pancancer data retrieval functions to data_utilities

--- pancancer_utilities/data_utilities.py
+++ pancancer_utilities/data_utilities.py
@@ -43,6 +43,75 @@ def load_expression_data(scale_input=False, verbose=False):
 
     return rnaseq_df
 
+def load_pancancer_data(gene_list, verbose=False):
+    # load data (TODO: what data is this?)
+    if verbose:
+        print('Loading gene label data...', file=sys.stderr)
+
+    genes_df = load_top_50()
+    if gene_list is not None:
+        genes_df = genes_df[genes_df['gene'].isin(gene_list)]
+        genes_df.reset_index(drop=True, inplace=True)
+
+    # loading this data from the pancancer repo is very slow, so we
+    # cache it in a pickle to speed up loading
+    if os.path.exists(cfg.pancan_data):
+        if verbose:
+            print('Loading pan-cancer data from cached pickle file...', file=sys.stderr)
+        with open(cfg.pancan_data, 'rb') as f:
+            pancan_data = pkl.load(f)
+    else:
+        if verbose:
+            print('Loading pan-cancer data from repo (warning: slow)...', file=sys.stderr)
+        pancan_data = load_pancancer_data_from_repo()
+        with open(cfg.pancan_data, 'wb') as f:
+            pkl.dump(pancan_data, f)
+
+    return (genes_df, pancan_data)
+
+def load_top_50():
+    """Load top 50 mutated genes in TCGA from BioBombe repo.
+
+    These were precomputed for the equivalent experiments in the
+    BioBombe paper, so no need to recompute them.
+    """
+    base_url = "https://github.com/greenelab/BioBombe/raw"
+    commit = "aedc9dfd0503edfc5f25611f5eb112675b99edc9"
+
+    file = "{}/{}/9.tcga-classify/data/top50_mutated_genes.tsv".format(
+            base_url, commit)
+    genes_df = pd.read_csv(file, sep='\t')
+    return genes_df
+
+def load_pancancer_data_from_repo():
+    """Load data to build feature matrices from pancancer repo. """
+
+    base_url = "https://github.com/greenelab/pancancer/raw"
+    commit = "2a0683b68017fb226f4053e63415e4356191734f"
+
+    file = "{}/{}/data/sample_freeze.tsv".format(base_url, commit)
+    sample_freeze_df = pd.read_csv(file, index_col=0, sep='\t')
+
+    file = "{}/{}/data/pancan_mutation_freeze.tsv.gz".format(base_url, commit)
+    mutation_df = pd.read_csv(file, index_col=0, sep='\t')
+
+    file = "{}/{}/data/copy_number_loss_status.tsv.gz".format(base_url, commit)
+    copy_loss_df = pd.read_csv(file, index_col=0, sep='\t')
+
+    file = "{}/{}/data/copy_number_gain_status.tsv.gz".format(base_url, commit)
+    copy_gain_df = pd.read_csv(file, index_col=0, sep='\t')
+
+    file = "{}/{}/data/mutation_burden_freeze.tsv".format(base_url, commit)
+    mut_burden_df = pd.read_csv(file, index_col=0, sep='\t')
+
+    return (
+        sample_freeze_df,
+        mutation_df,
+        copy_loss_df,
+        copy_gain_df,
+        mut_burden_df
+    )
+
 def split_by_cancer_type(rnaseq_df, sample_info_df, holdout_cancer_type,
                          use_pancancer=False, num_folds=4, fold_no=1,
                          seed=cfg.default_seed):
