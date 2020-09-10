434a2346187e55e180030d779bfb3123a90927b0
Jake Crawford <jjc2718@gmail.com>
2020-09-10 15:17:19 -0400

    add code for statistical testing and plot metric change vs. p-value

--- pancancer_evaluation/utilities/analysis_utilities.py
+++ pancancer_evaluation/utilities/analysis_utilities.py
@@ -1,6 +1,10 @@
 import os
+import sys
 
+import numpy as np
 import pandas as pd
+# TODO should this be a paired t-test?
+from scipy.stats import ttest_ind
 
 def load_prediction_results(results_dir, train_set_descriptor):
     """Load results of mutation prediction experiments.
@@ -29,3 +33,47 @@ def load_prediction_results(results_dir, train_set_descriptor):
             )
             results_df = pd.concat((results_df, gene_results_df))
     return results_df
+
+def compare_results(results_df,
+                    identifier='gene',
+                    metric='auroc',
+                    correction=False,
+                    correction_method='fdr_bh',
+                    correction_alpha=0.05,
+                    verbose=False):
+    """which gene/cancer type combinations beat the negative control baseline?
+
+    TODO better documentation
+    """
+    results = []
+    unique_identifiers = np.unique(results_df[identifier].values)
+    for id_str in unique_identifiers:
+        signal_results = results_df[
+            (results_df[identifier] == id_str) &
+            (results_df.data_type == 'test') &
+            (results_df.signal == 'signal')
+        ][metric].values
+        shuffled_results = results_df[
+            (results_df[identifier] == id_str) &
+            (results_df.data_type == 'test') &
+            (results_df.signal == 'shuffled')
+        ][metric].values
+        if signal_results.shape != shuffled_results.shape:
+            if verbose:
+                print('shapes unequal for {}, skipping'.format(id_str),
+                      file=sys.stderr)
+            continue
+        delta_mean = np.mean(signal_results) - np.mean(shuffled_results)
+        p_value = ttest_ind(signal_results, shuffled_results)[1]
+        results.append([id_str, delta_mean, p_value])
+    results_df =  pd.DataFrame(results, columns=['identifier', 'delta_mean', 'p_value'])
+
+    if correction:
+        from statsmodels.stats.multitest import multipletests
+        corr = multipletests(results_df['p_value'],
+                             alpha=correction_alpha,
+                             method=correction_method)
+        results_df = results_df.assign(corr_pval=corr[1], reject_null=corr[0])
+
+    return results_df
+
