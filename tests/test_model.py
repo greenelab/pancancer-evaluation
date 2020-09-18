8607cf0de93f330b446df1ea1e99c2e8a07e9ba8
Jake Crawford <jjc2718@gmail.com>
2020-09-18 13:18:05 -0400

    started to work on a regression test for model implementation
    still need to write code to generate fixtures, to compare against

---
+++ tests/test_model.py
@@ -0,0 +1,37 @@
+"""
+Test cases for model fitting code in classify_utilities.py
+"""
+import pytest
+import pandas as pd
+
+import pancancer_evaluation.config as cfg
+from pancancer_evaluation.data_models.tcga_data_model import TCGADataModel
+import pancancer_evaluation.utilities.classify_utilities as cu
+import pancancer_evaluation.utilities.data_utilities as du
+
+@pytest.fixture(scope='module')
+def data_model():
+    """Load data model and sample info data"""
+    # TODO: define results dir?
+    tcga_data = TCGADataModel(debug=True)
+    sample_info_df = du.load_sample_info()
+    return tcga_data, sample_info_df
+
+@pytest.mark.parametrize("gene_info", [('TP53', 'TSG'),
+                                       ('KRAS', 'oncogene'),
+                                       ('ARID1A', 'TSG')])
+def test_stratified_prediction(data_model, gene_info):
+    """Regression test for prediction using stratified cross-validation"""
+    tcga_data, sample_info_df = data_model
+    gene, classification = gene_info
+    tcga_data.process_data_for_gene(gene, classification,
+                                    check_gene_file=True,
+                                    shuffle_labels=False)
+    results = cu.run_cv_stratified(tcga_data, gene, sample_info_df,
+                                   num_folds=4, shuffle_labels=False)
+    assert results
+
+def test_cancer_type_prediction(data_model, gene_info):
+    """Regression test for prediction using cancer type cross-validation"""
+    assert True
+
