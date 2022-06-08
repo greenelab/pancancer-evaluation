5936d188ee040d85469f2d4ce725bdcd00e73fec
Jake Crawford <jjc2718@gmail.com>
2022-06-08 15:43:37 -0400

    add facet plots to p53 analysis nb

--- 03_cross_cancer_classification/nbconverted/p53_stability_pilot.py
+++ 03_cross_cancer_classification/nbconverted/p53_stability_pilot.py
@@ -34,7 +34,7 @@ print(cross_cancer_df.shape)
 cross_cancer_df.head()
 
 
-# In[8]:
+# In[4]:
 
 
 heatmap_df, sorted_ids = au.heatmap_from_results(cross_cancer_df,
@@ -43,7 +43,7 @@ heatmap_df, sorted_ids = au.heatmap_from_results(cross_cancer_df,
 heatmap_df.iloc[:5, :5]
 
 
-# In[9]:
+# In[5]:
 
 
 sns.set({'figure.figsize': (15, 8)})
@@ -51,4 +51,100 @@ sns.heatmap(heatmap_df, cbar_kws={'label': 'AUPR difference from baseline'}, cen
 plt.title('Cross-cancer mutation detection, AUPR heatmap')
 plt.xlabel('Test identifier')
 plt.ylabel('Train identifier')
+
+
+# In[6]:
+
+
+def get_valid_scores(results_dir):
+    best_params_df = []
+    for results_file in os.listdir(results_dir):
+        if 'param_grid' not in results_file: continue
+        if results_file[0] == '.': continue
+        train_identifier = results_file.split('.')[0]
+        test_identifier = results_file.split('.')[1]
+        signal = results_file.split('.')[2].split('_')[0]
+        seed = int(results_file.split('.')[2].split('_')[1].replace('s', ''))
+        params_df = pd.read_csv(
+            os.path.join(results_dir, results_file),
+            sep='\t', index_col=['alpha', 'l1_ratio']
+        )
+        params_df.drop(columns=['Unnamed: 0'], inplace=True)
+        best_params = (params_df
+            .sort_values(by='mean_test_score', ascending=False)
+            .head(1)
+        )
+        best_params['train_identifier'] = train_identifier
+        best_params['test_identifier'] = test_identifier
+        best_params['signal'] = signal
+        best_params['seed'] = seed
+        best_params_df.append(best_params.reset_index())
+    return pd.concat(best_params_df).reset_index()
+
+inner_results_df = get_valid_scores(p53_results_dir)
+print(inner_results_df.shape)
+inner_results_df.head()
+
+
+# In[7]:
+
+
+plot_df = (inner_results_df
+    .drop(columns=['index', 'alpha', 'l1_ratio', 'fold', 'loss', 'penalty', 'mean_train_score'])
+    .rename(columns={'mean_test_score': 'aupr'})
+)
+plot_df['split'] = 'valid'
+plot_df.head()
+
+
+# In[8]:
+
+
+plot_big_df = pd.concat((
+    plot_df,
+    cross_cancer_df[cross_cancer_df.data_type == 'test']
+      .drop(columns=['auroc', 'data_type', 'experiment'])
+      .assign(split='test')
+))
+plot_big_df['identifiers'] = (
+    plot_big_df.train_identifier + '/' + plot_big_df.test_identifier
+)
+plot_big_df = (
+    plot_big_df[plot_big_df.signal == 'signal']
+      .sort_values(by=['identifiers', 'split'], ascending=[True, False])
+)
+plot_big_df.head()
+
+
+# In[9]:
+
+
+g = sns.catplot(
+    data=plot_big_df,
+    x='split', y='aupr', col='identifiers', kind='box', col_wrap=8,
+    height=2.5, aspect=1.35
+)
+g.set_titles(col_template='{col_name}')
+
+
+# In[10]:
+
+
+g = sns.catplot(
+    data=plot_big_df,
+    x='split', y='aupr', col='identifiers', kind='point', col_wrap=8,
+    height=2.5, aspect=1.35
+)
+g.set_titles(col_template='{col_name}')
+
+
+# In[11]:
+
+
+g = sns.catplot(
+    data=plot_big_df,
+    x='split', y='aupr', col='identifiers', kind='point', hue='seed', col_wrap=8,
+    color='green', ci=None, height=2.5, aspect=1.35
+)
+g.set_titles(col_template='{col_name}')
 
