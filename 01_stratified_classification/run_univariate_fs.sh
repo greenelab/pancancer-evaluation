9c6c9ab4eb1c269e0212b4c98a1d30039082a7dc
Jake Crawford <jjc2718@gmail.com>
2022-07-22 13:06:56 -0400

    add script for cancer type feature selection

--- 01_stratified_classification/run_univariate_fs.sh
+++ 01_stratified_classification/run_univariate_fs.sh
@@ -2,13 +2,13 @@
 
 # Run feature selection experiments across a few genes
 
-RESULTS_DIR=./01_stratified_classification/results/univariate_fs
+RESULTS_DIR=./01_stratified_classification/results/univariate_fs_big
 ERRORS_DIR=./stratified_fs_errors
 
 mkdir -p $ERRORS_DIR
 
 genes=(
-  # "TP53"
+  "TP53"
   "APC"
   "EGFR"
   "SETD2"
@@ -26,7 +26,7 @@ for seed in 42 1; do
         cmd+="--results_dir $RESULTS_DIR "
         cmd+="--seed $seed "
         cmd+="--feature_selection mad "
-        cmd+="--num_features 1000 "
+        cmd+="--num_features 8000 "
         cmd+="2>$ERRORS_DIR/errors_${gene}_${seed}_mad.txt"
         echo "Running: $cmd"
         eval $cmd
@@ -37,8 +37,8 @@ for seed in 42 1; do
         cmd+="--results_dir $RESULTS_DIR "
         cmd+="--seed $seed "
         cmd+="--feature_selection mad "
-        cmd+="--num_features 100 "
-        cmd+="2>$ERRORS_DIR/errors_${gene}_${seed}_mad_small.txt"
+        cmd+="--num_features 1000 "
+        cmd+="2>$ERRORS_DIR/errors_${gene}_${seed}_mad.txt"
         echo "Running: $cmd"
         eval $cmd
 
@@ -48,7 +48,7 @@ for seed in 42 1; do
         cmd+="--results_dir $RESULTS_DIR "
         cmd+="--seed $seed "
         cmd+="--feature_selection pancan_f_test "
-        cmd+="--num_features 100 "
+        cmd+="--num_features 1000 "
         cmd+="2>$ERRORS_DIR/errors_${gene}_${seed}_pancan_f_test.txt"
         echo "Running: $cmd"
         eval $cmd
@@ -59,7 +59,7 @@ for seed in 42 1; do
         cmd+="--results_dir $RESULTS_DIR "
         cmd+="--seed $seed "
         cmd+="--feature_selection median_f_test "
-        cmd+="--num_features 100 "
+        cmd+="--num_features 1000 "
         cmd+="2>$ERRORS_DIR/errors_${gene}_${seed}_median_f_test.txt"
         echo "Running: $cmd"
         eval $cmd
@@ -70,7 +70,7 @@ for seed in 42 1; do
         cmd+="--results_dir $RESULTS_DIR "
         cmd+="--seed $seed "
         cmd+="--feature_selection mad_f_test "
-        cmd+="--num_features 100 "
+        cmd+="--num_features 1000 "
         cmd+="2>$ERRORS_DIR/errors_${gene}_${seed}_mad_f_test.txt"
         echo "Running: $cmd"
         eval $cmd
---
+++ 02_cancer_type_classification/run_univariate_fs.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+# Run feature selection experiments across a few genes, with all cancer
+# types held out
+
+RESULTS_DIR=./02_cancer_type_classification/results/univariate_fs
+ERRORS_DIR=./cancer_type_fs_errors
+
+mkdir -p $ERRORS_DIR
+
+genes=(
+  "TP53"
+  "APC"
+  "EGFR"
+  "SETD2"
+  "ARID1A"
+  "PIK3CA"
+)
+
+fs_methods=(
+  "mad"
+  "pancan_f_test"
+  "median_f_test"
+  "mad_f_test"
+)
+
+for seed in 42 1; do
+
+    for gene in "${genes[@]}"; do
+
+        # select to 8000 features as performance "upper bound"
+        cmd="python 02_cancer_type_classification/run_cancer_type_classification.py "
+        cmd+="--gene_set custom "
+        cmd+="--custom_genes $gene "
+        cmd+="--results_dir $RESULTS_DIR "
+        cmd+="--seed $seed "
+        cmd+="--feature_selection mad "
+        cmd+="--num_features 8000 "
+        cmd+="2>$ERRORS_DIR/errors_${gene}_${seed}_mad.txt"
+        echo "Running: $cmd"
+        eval $cmd
+
+        cmd="python 02_cancer_type_classification/run_cancer_type_classification.py "
+        cmd+="--gene_set custom "
+        cmd+="--custom_genes $gene "
+        cmd+="--results_dir $RESULTS_DIR "
+        cmd+="--seed $seed "
+        cmd+="--feature_selection mad "
+        cmd+="--num_features 8000 "
+        cmd+="--pancancer_only "
+        cmd+="2>$ERRORS_DIR/errors_${gene}_${seed}_mad_pancancer_only.txt"
+        echo "Running: $cmd"
+        eval $cmd
+
+        for fs_method in "${fs_methods[@]}"; do
+
+            # select to 1000 features with each feature selection method
+            cmd="python 02_cancer_type_classification/run_cancer_type_classification.py "
+            cmd+="--gene_set custom "
+            cmd+="--custom_genes $gene "
+            cmd+="--results_dir $RESULTS_DIR "
+            cmd+="--seed $seed "
+            cmd+="--feature_selection $fs_method "
+            cmd+="--num_features 1000 "
+            cmd+="2>$ERRORS_DIR/errors_${gene}_${seed}_${fs_method}.txt"
+            echo "Running: $cmd"
+            eval $cmd
+
+            cmd="python 02_cancer_type_classification/run_cancer_type_classification.py "
+            cmd+="--gene_set custom "
+            cmd+="--custom_genes $gene "
+            cmd+="--results_dir $RESULTS_DIR "
+            cmd+="--seed $seed "
+            cmd+="--feature_selection $fs_method "
+            cmd+="--num_features 1000 "
+            cmd+="--pancancer_only "
+            cmd+="2>$ERRORS_DIR/errors_${gene}_${seed}_${fs_method}_pancancer_only.txt"
+            echo "Running: $cmd"
+            eval $cmd
+
+        done
+
+    done
+
+done
