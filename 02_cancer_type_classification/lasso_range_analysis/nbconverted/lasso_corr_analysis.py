855975738eb341b18cc390e68d3c57836ac98cab
Jake Crawford <jjc2718@gmail.com>
2023-01-30 12:54:06 -0500

    changes to lasso_corr_analysis

--- 02_cancer_type_classification/lasso_range_analysis/nbconverted/lasso_corr_analysis.py
+++ 02_cancer_type_classification/lasso_range_analysis/nbconverted/lasso_corr_analysis.py
@@ -3,7 +3,7 @@
 
 # ## Compare model size/performance correlations using different measures
 
-# In[16]:
+# In[1]:
 
 
 import numpy as np
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# In[9]:
+# In[2]:
 
 
 corr_methods = ['pearson', 'spearman', 'ccc']
@@ -37,7 +37,7 @@ print(corr_methods_df.shape)
 corr_methods_df.sort_values(by=['gene', 'cancer_type']).head(10)
 
 
-# In[10]:
+# In[3]:
 
 
 # want to find examples where the correlation methods disagree
@@ -52,9 +52,52 @@ corr_wide_df = (corr_methods_df
 corr_wide_df.head()
 
 
-# In[23]:
+# ### Compare using absolute differences between Pearson/Spearman and CCC
+# 
+# Large difference = CCC disagrees more with Pearson/Spearman
+
+# In[4]:
 
 
+gene_diff_df = (corr_wide_df
+    .reset_index()
+    .groupby('gene')
+    .agg(np.median)
+)
+    
+gene_diff_df.sort_values(by=['pearson'], ascending=True).head()
+
+
+# In[5]:
+
+
+gene_diff_df['ccc_pearson_diff'] = (
+    gene_diff_df.ccc - gene_diff_df.pearson
+).abs()
+
+gene_diff_df.sort_values(by='ccc_pearson_diff', ascending=False).head(8)
+
+
+# In[6]:
+
+
+gene_diff_df['ccc_spearman_diff'] = (
+    gene_diff_df.ccc - gene_diff_df.spearman
+).abs()
+
+gene_diff_df.sort_values(by='ccc_spearman_diff', ascending=False).head(8)
+
+
+# ### Compare using rank differences between Pearson/Spearman and CCC
+# 
+# The above analyses tend to skew toward genes with large baseline correlations, we want to see if taking ranks will result in different top hits.
+
+# In[7]:
+
+
+# take rank of median correlation across cancer types
+# spearman/pearson and CCC have slightly different scales (s/p are
+# in [-1, 1] and CCC is in [0, 1] so comparing ranks could make more sense
 gene_ranks_df = (corr_wide_df
     .reset_index()
     .groupby('gene')
@@ -65,7 +108,7 @@ gene_ranks_df = (corr_wide_df
 gene_ranks_df.sort_values(by=['pearson'], ascending=True).head()
 
 
-# In[24]:
+# In[8]:
 
 
 gene_ranks_df['ccc_pearson_diff'] = (
@@ -75,7 +118,7 @@ gene_ranks_df['ccc_pearson_diff'] = (
 gene_ranks_df.sort_values(by='ccc_pearson_diff', ascending=False).head(8)
 
 
-# In[25]:
+# In[9]:
 
 
 gene_ranks_df['ccc_spearman_diff'] = (
@@ -84,3 +127,7 @@ gene_ranks_df['ccc_spearman_diff'] = (
 
 gene_ranks_df.sort_values(by='ccc_spearman_diff', ascending=False).head(8)
 
+
+# Overall we see that some of the same genes show up (e.g. CDH1) but there are some genes with low overall ranks but high differences (e.g. ATRX, H3F3A).
+# 
+# Most of the genes that show up in the absolute difference lists are genes that we've previously seen accurate mutation prediction for (TP53, RB1, ARID1A, SETD2).
