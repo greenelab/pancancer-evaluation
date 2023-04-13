56b8d66665cf5adfdbe2b45a5686a1328acc199e
Jake Crawford <jjc2718@gmail.com>
2023-04-13 10:18:14 -0400

    load coef information into gene script

--- 01_stratified_classification/nbconverted/lasso_range_gene_optimizers.py
+++ 01_stratified_classification/nbconverted/lasso_range_gene_optimizers.py
@@ -42,73 +42,119 @@ metric = 'aupr'
 
 
 # ### Get coefficient information for each lasso penalty
-nz_coefs_df = []
+
+# In[3]:
+
+
+ll_nz_coefs_df = []
 
 # get coefficient info for training dataset specified above
-for coef_info in au.generate_nonzero_coefficients_lasso_range(results_dir, gene=plot_gene):
+for coef_info in au.generate_nonzero_coefficients_lasso_range(ll_results_dir, gene=plot_gene):
     (gene,
      cancer_type,
      seed,
      lasso_param,
      coefs_list) = coef_info
     for fold_no, coefs in enumerate(coefs_list):
-        nz_coefs_df.append(
+        ll_nz_coefs_df.append(
             [gene, cancer_type, lasso_param, seed, fold_no, len(coefs)]
         )
         
-nz_coefs_df = pd.DataFrame(
-    nz_coefs_df,
-    columns=['gene', 'cancer_type', 'lasso_param', 'seed', 'fold', 'nz_coefs']
+ll_nz_coefs_df = pd.DataFrame(
+    ll_nz_coefs_df,
+    columns=['gene', 'signal', 'lasso_param', 'seed', 'fold', 'nz_coefs']
 )
-nz_coefs_df.lasso_param = nz_coefs_df.lasso_param.astype(float)
-nz_coefs_df = nz_coefs_df[nz_coefs_df.gene == plot_gene].copy()
-nz_coefs_df.head()sns.set({'figure.figsize': (12, 5)})
+ll_nz_coefs_df.lasso_param = ll_nz_coefs_df.lasso_param.astype(float)
+ll_nz_coefs_df = ll_nz_coefs_df[ll_nz_coefs_df.gene == plot_gene].copy()
+ll_nz_coefs_df.head()
+
+
+# In[4]:
+
+
+sgd_nz_coefs_df = []
+
+# get coefficient info for training dataset specified above
+for coef_info in au.generate_nonzero_coefficients_lasso_range(sgd_results_dir, gene=plot_gene):
+    (gene,
+     cancer_type,
+     seed,
+     lasso_param,
+     coefs_list) = coef_info
+    for fold_no, coefs in enumerate(coefs_list):
+        sgd_nz_coefs_df.append(
+            [gene, cancer_type, lasso_param, seed, fold_no, len(coefs)]
+        )
+        
+sgd_nz_coefs_df = pd.DataFrame(
+    sgd_nz_coefs_df,
+    columns=['gene', 'signal', 'lasso_param', 'seed', 'fold', 'nz_coefs']
+)
+sgd_nz_coefs_df.lasso_param = sgd_nz_coefs_df.lasso_param.astype(float)
+sgd_nz_coefs_df = sgd_nz_coefs_df[sgd_nz_coefs_df.gene == plot_gene].copy()
+sgd_nz_coefs_df.head()
+
+
+# In[5]:
+
+
+sns.set({'figure.figsize': (12, 10)})
 sns.set_style('whitegrid')
 
+fig, axarr = plt.subplots(2, 1)
+
 sns.boxplot(
-    data=nz_coefs_df.sort_values(by=['cancer_type', 'lasso_param']),
-    x='cancer_type', y='nz_coefs', hue='lasso_param'
+    data=ll_nz_coefs_df.sort_values(by=['lasso_param']),
+    x='lasso_param', y='nz_coefs', ax=axarr[0]
 )
+axarr[0].set_title('liblinear optimizer', size=16)
+axarr[0].set_xlabel('')
+axarr[0].set_ylabel('Number of nonzero coefficients', size=13)
+axarr[0].tick_params(axis='both', labelsize=12)
+
+sns.boxplot(
+    data=sgd_nz_coefs_df.sort_values(by=['lasso_param']),
+    x='lasso_param', y='nz_coefs', ax=axarr[1]
+)
+axarr[1].set_title('SGD optimizer', size=16)
+axarr[1].set_xlabel('LASSO parameter', size=13)
+axarr[1].set_ylabel('Number of nonzero coefficients', size=13)
+axarr[1].tick_params(axis='both', labelsize=12)
 
 # color the boxplot lines/edges rather than the box fill
 # this makes it easier to discern colors at the extremes; i.e. very many or few nonzero coefs
 # https://stackoverflow.com/a/72333641
-ax = plt.gca()
-box_patches = [patch for patch in ax.patches if type(patch) == matplotlib.patches.PathPatch]
-num_patches = len(box_patches)
-lines_per_boxplot = len(ax.lines) // num_patches
-for i, patch in enumerate(box_patches):
-    # set the linecolor on the patch to the facecolor, and set the facecolor to None
-    col = patch.get_facecolor()
-    patch.set_edgecolor(col)
-    patch.set_facecolor('None')
+def color_boxes(ax):
+    box_patches = [patch for patch in ax.patches if type(patch) == matplotlib.patches.PathPatch]
+    num_patches = len(box_patches)
+    lines_per_boxplot = len(ax.lines) // num_patches
+    for i, patch in enumerate(box_patches):
+        # set the linecolor on the patch to the facecolor, and set the facecolor to None
+        col = patch.get_facecolor()
+        patch.set_edgecolor(col)
+        patch.set_facecolor('None')
 
-    # each box has associated Line2D objects (to make the whiskers, fliers, etc.)
-    # loop over them here, and use the same color as above
-    for line in ax.lines[i * lines_per_boxplot: (i + 1) * lines_per_boxplot]:
-        line.set_color(col)
-        line.set_mfc(col)  # facecolor of fliers
-        line.set_mec(col)  # edgecolor of fliers
+        # each box has associated Line2D objects (to make the whiskers, fliers, etc.)
+        # loop over them here, and use the same color as above
+        for line in ax.lines[i * lines_per_boxplot: (i + 1) * lines_per_boxplot]:
+            line.set_color(col)
+            line.set_mfc(col)  # facecolor of fliers
+            line.set_mec(col)  # edgecolor of fliers
+            
+color_boxes(axarr[0])
+color_boxes(axarr[1])
 
-# also fix the legend to color the edges rather than fill
-for legpatch in ax.legend_.get_patches():
-    col = legpatch.get_facecolor()
-    legpatch.set_edgecolor(col)
-    legpatch.set_facecolor('None')
+plt.suptitle(
+    f'LASSO parameter vs. number of nonzero coefficients, {plot_gene}',
+    size=18, y=0.995
+)
 
-sns.move_legend(ax, "upper left", bbox_to_anchor=(1, 1))
-plt.title(f'LASSO parameter vs. number of nonzero coefficients, {plot_gene}',
-          size=16)
-plt.xlabel('Cancer type', size=14)
-plt.ylabel('Number of nonzero coefficients', size=14)
-_, xlabels = plt.xticks()
-_ = ax.set_xticklabels(xlabels, size=12)
-ax.set_yticks(ax.get_yticks()[1:])
-_ = ax.set_yticklabels(ax.get_yticks(), size=12)
 plt.tight_layout()
+
+
 # ### Get performance information for each lasso penalty
 
-# In[3]:
+# In[6]:
 
 
 ll_perf_df = au.load_prediction_results_lasso_range(ll_results_dir,
@@ -122,7 +168,7 @@ ll_perf_df = (
 ll_perf_df.head()
 
 
-# In[4]:
+# In[7]:
 
 
 sgd_perf_df = au.load_prediction_results_lasso_range(sgd_results_dir,
@@ -136,7 +182,7 @@ sgd_perf_df = (
 sgd_perf_df.head()
 
 
-# In[5]:
+# In[8]:
 
 
 # same plot as before but with the "best"/"smallest" parameters marked
