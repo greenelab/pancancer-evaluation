f56888f65d8b09d3a9b09e91367ee6d9fb51135d
Jake Crawford <jjc2718@gmail.com>
2023-06-15 13:20:32 -0400

    other small changes

--- 08_cell_line_prediction/nn_diagnostics/nbconverted/nn_hsize_range_all.py
+++ 08_cell_line_prediction/nn_diagnostics/nbconverted/nn_hsize_range_all.py
@@ -179,3 +179,110 @@ with sns.plotting_context('notebook', font_scale=1.6):
     plt.suptitle('Hidden layer size vs. performance above/below median rank', y=1.05)
     sns.move_legend(g, 'center right', frameon=True)
 
+
+# In[10]:
+
+
+def get_top_and_smallest_diff(gene):
+    top_df = (
+        hsize_df[(hsize_df.gene == gene) &
+                 (hsize_df.data_type == 'cv') &
+                 (hsize_df.signal == 'signal')]
+          .groupby(['hsize'])
+          .agg(np.mean)
+          .drop(columns=['seed', 'fold'])
+          .rename(columns={'auroc': 'mean_auroc', 'aupr': 'mean_aupr'})
+          .sort_values(by='mean_aupr', ascending=False)
+    )
+    top_df['aupr_rank'] = top_df.mean_aupr.rank(ascending=False)
+    top_5_hsize = top_df.loc[top_df.aupr_rank <= 5, :].index
+
+    # get parameter with best validation performance
+    top_hsize = top_5_hsize[0]
+
+    # get smallest hsize in top 5 validation performance
+    smallest_hsize = top_5_hsize.min()
+
+    holdout_df = (
+        hsize_df[(hsize_df.gene == gene) &
+                 (hsize_df.data_type == 'test') &
+                 (hsize_df.signal == 'signal')]
+          .groupby(['hsize'])
+          .agg(np.mean)
+          .drop(columns=['seed', 'fold'])
+          .rename(columns={'auroc': 'mean_auroc', 'aupr': 'mean_aupr'})
+    )
+
+    top_smallest_diff = (
+        holdout_df.loc[top_hsize, 'mean_aupr'] -
+        holdout_df.loc[smallest_hsize, 'mean_aupr']
+    )
+    return [gene, top_hsize, smallest_hsize, top_smallest_diff]
+
+print(get_top_and_smallest_diff('SETD2'))
+
+
+# In[11]:
+
+
+all_top_smallest_diff_df = []
+
+for gene in hsize_df.gene.unique():
+    all_top_smallest_diff_df.append(get_top_and_smallest_diff(gene))
+        
+all_top_smallest_diff_df = pd.DataFrame(
+    all_top_smallest_diff_df,
+    columns=['gene', 'top_hsize', 'smallest_hsize', 'top_smallest_diff']
+)
+all_top_smallest_diff_df['best'] = 'top'
+all_top_smallest_diff_df.loc[
+    all_top_smallest_diff_df.top_smallest_diff < 0, 'best'
+] = 'smallest'
+all_top_smallest_diff_df.loc[
+    all_top_smallest_diff_df.top_smallest_diff == 0, 'best'
+] = 'zero'
+
+print(all_top_smallest_diff_df.best.value_counts())
+all_top_smallest_diff_df.head()
+
+
+# In[12]:
+
+
+sns.set({'figure.figsize': (8, 6)})
+sns.set_style('whitegrid')
+
+sns.histplot(all_top_smallest_diff_df.top_smallest_diff, bins=20)
+plt.xlim(-0.2, 0.2)
+plt.title('Differences between "best" and "smallest good" hidden layer size')
+plt.xlabel('AUPR(best) - AUPR(smallest good)')
+plt.gca().axvline(0, color='black', linestyle='--')
+
+
+# In[13]:
+
+
+sns.set({'figure.figsize': (8, 6)})
+sns.set_style('whitegrid')
+
+sns.histplot(
+    all_top_smallest_diff_df[all_top_smallest_diff_df.top_smallest_diff != 0.0].top_smallest_diff,
+    bins=20
+)
+plt.xlim(-0.2, 0.2)
+plt.title('Differences between "best" and "smallest good" hidden layer size, without zeroes')
+plt.xlabel('AUPR(best) - AUPR(smallest good)')
+plt.gca().axvline(0, color='black', linestyle='--')
+
+
+# In[14]:
+
+
+all_top_smallest_diff_df.sort_values(by='top_smallest_diff', ascending=False).head(10)
+
+
+# In[15]:
+
+
+all_top_smallest_diff_df.sort_values(by='top_smallest_diff', ascending=True).head(10)
+
